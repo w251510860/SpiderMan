c6c61b936a07629865f0242f51456e3ea86743cc
爆炒八酱 <251510860@qq.com>
2022-09-30 16:58:04 +0800

    更新框架，增加白名单过滤保存内容

--- SpiderMan/pipeline/procurement_pipeline.py
+++ SpiderMan/pipeline/procurement_pipeline.py
@@ -13,9 +13,14 @@ class ProcurementSaveMongoPipeline:
 
     def process_item(self, item, spider):
         item = dict(item)
+        white_list = ['集采', '采购', '投标', '中标', '招标', '谈判', '候选', '谈判', '供应', '合同', '产品',
+                      '器械', '耗材', '设备', '成交', '竞争', '预算', '磋商', '限价']
         item["digest"] = item['hospital_name'] + item['title'] + item['ori_url']
-        filter = {"digest": item["digest"]}
-        self.col.update_one(filter, {'$setOnInsert': item}, upsert=True)
+        for key_word in white_list:
+            if key_word in item['mainbody']:
+                filter = {"digest": item["digest"]}
+                self.col.update_one(filter, {'$setOnInsert': item}, upsert=True)
+                return item
         return item
 
 
